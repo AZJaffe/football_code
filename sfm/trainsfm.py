7226c0ff489ccccec1717a834daa9e9b9d0f7976
Adam Jaffe <azjaffe1@gmail.com>
2020-07-17 13:57:55 -0400

    Use fire instead of parseargs

--- sfm/trainsfm.py
+++ sfm/trainsfm.py
@@ -1,5 +1,6 @@
 import argparse
 import datetime
+import fire
 import os
 import matplotlib.pyplot as plt
 import numpy as np
@@ -34,22 +35,27 @@ def save(checkpoint_file, model, optimizer, e):
   os.replace(tmp_file, checkpoint_file)
   print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Checkpoint saved at {checkpoint_file}')
 
-def train(*, 
+def train(*,
+  data_dir,
   tensorboard_dir,
-  ds, 
-  lr=0.001,
-  num_epochs=1, 
-  flow_reg_coeff=0.,
-  batch_size=16, 
-  checkpoint_freq=100,
   checkpoint_file,
-  device=torch.device('cpu'),
+  checkpoint_freq=100,
+  load_data_to_device=True,
+  disable_cuda=False,
+  K=1,
   fc_layer_width=128,
   conv_depth=2,
-  K=1
+  lr=0.001,
+  flow_reg_coeff=0.,
+  batch_size=16, 
+  num_epochs=1, 
 ):
-  if ds is None:
-    raise 'Pass a dataset'
+  if disable_cuda is False and torch.cuda.is_available():
+    device = torch.device('cuda')                  
+  else:                                                   
+    device = torch.device('cpu')
+
+  ds=PairConsecutiveFramesDataset(data_dir, load_all=load_data_to_device, device=device)
 
   input_shape = ds[0].shape
   model = sfmnet.SfMNet(H=input_shape[1], W=input_shape[2], K=K, conv_depth=conv_depth, fc_layer_width=fc_layer_width)
@@ -59,7 +65,7 @@ def train(*,
   if checkpoint_file is not None:
     start_epoch = load(checkpoint_file, model, optimizer)
 
-  dl = torch.utils.data.DataLoader(ds, batch_size=args.batch_size, shuffle=True)
+  dl = torch.utils.data.DataLoader(ds, batch_size=batch_size, shuffle=True)
   
   def loss_fn(input, output, masks, flow, displacements):
     recon_loss = sfmnet.l1_recon_loss(input[:,3:6], output)
@@ -73,6 +79,14 @@ def train(*,
   with SummaryWriter(tensorboard_dir) as writer:
     writer.add_graph(model, ds[0].unsqueeze(0))
     writer.add_text('model_summary', str(model))
+    writer.add_scalars('hparams', {
+      'lr':lr,
+      'batch_size': batch_size,
+      'flow_reg_coeff': flow_reg_coeff,
+      'conv_depth': conv_depth,
+      'fc_layer_width': fc_layer_width,
+      'K': K,
+    })
     for e in range(start_epoch, start_epoch+num_epochs):
       epoch_start_time = time.monotonic()
       total_loss = 0.
@@ -110,54 +124,4 @@ def train(*,
   save(checkpoint_file, model, optimizer, start_epoch+num_epochs)
 
 if __name__=='__main__':
-  # Parse args
-  parser = argparse.ArgumentParser(description='Train an sfm')
-  parser.add_argument('--data_dir', required=True,
-                      help='the directory containing sequential images to use as data')
-  parser.add_argument('--tensorboard_dir',
-                      help='the directory to save tensorboard events')
-  parser.add_argument('--checkpoint_freq', type=int, default=100,
-                      help='the frequency in epochs of saving the model')
-  parser.add_argument('--checkpoint_file',
-                      help='the directory to save checkpoints')
-                      
-  parser.add_argument('--lr', type=float, default=0.001,
-                      help='the learning rate of the Adam optimizer')
-  parser.add_argument('--flow_reg_coeff', type=float, default=0.,
-                      help='the flow regularization coefficient')
-  parser.add_argument('--batch_size', type=int, default=16,
-                      help='the batch size used for training')
-  parser.add_argument('--num_epochs', type=int, default=2,
-                      help='the number of epochs to train for')
-  parser.add_argument('--disable_cuda', type=bool, default=False)
-
-  parser.add_argument('--fc_layer_width', type=int, default=128,
-                      help='the width of the fully connected layers of the model')
-  parser.add_argument('--conv_depth', type=int, default=2,
-                      help='the number of downsampling steps (1/2 of total number of convolutional encoding layers)')
-  parser.add_argument('--K', type=int, default=2,
-                      help='the number of objects to predict in the scene')
-
-  args = parser.parse_args()
-
-  if not args.disable_cuda and torch.cuda.is_available():
-    args.device = torch.device('cuda')                  
-  else:                                                   
-    args.device = torch.device('cpu')
-
-  print(f'using device type {args.device.type}')
-  print(args)
-
-  train(ds=PairConsecutiveFramesDataset(args.data_dir, load_all=True, device=args.device),
-        tensorboard_dir=args.tensorboard_dir,
-        checkpoint_file=args.checkpoint_file,
-        lr=args.lr,
-        num_epochs=args.num_epochs,
-        batch_size=args.batch_size,
-        checkpoint_freq=args.checkpoint_freq,
-        flow_reg_coeff=args.flow_reg_coeff,
-        device=args.device,
-        fc_layer_width=args.fc_layer_width,
-        conv_depth=args.conv_depth,
-        K=args.K
-  )
+  fire.Fire(train)
