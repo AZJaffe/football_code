e65afadf78ec4d64bf859dee9f71eddd59e84a74
Adam Jaffe <azjaffe1@gmail.com>
2020-09-11 12:30:06 -0400

    Add support for distributed training

--- sfm/trainsfm.py
+++ sfm/trainsfm.py
@@ -6,26 +6,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
 import math
 import pprint
 from torch.utils.tensorboard import SummaryWriter
+import functools
+
 
 import sfmnet
 from pair_frames_dataset import PairConsecutiveFramesDataset
 
-def load(checkpoint_file, model, optimizer):
+def load(checkpoint_file, model, optimizer, rank):
   if checkpoint_file is None:
-    return 0
+    return
   try:
     checkpoint = torch.load(checkpoint_file)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    map_location = {f'cuda:0': 'cuda:{rank}'}
+    model.load_state_dict(checkpoint['model_state_dict'], map_location=map_location)
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'], map_location=map_location)
     print(f'Loaded from checkpoint at {checkpoint_file}')
     return
   except FileNotFoundError:
     return
 
-def save(checkpoint_file, model, optimizer):
+def save(checkpoint_file, model, optimizer, rank):
+  if rank != 0:
+    return
   if checkpoint_file is None:
     return
   tmp_file = checkpoint_file + '.tmp'
@@ -53,6 +61,7 @@ def train_loop(*,
   maskvarreg_coeff=0.,
   num_epochs=1,
   epoch_callback=noop_callback,
+  using_ddp=False,
 ):
 
   step = 0
@@ -98,35 +107,37 @@ def train_loop(*,
     len_validate_ds = len(dl_validation.dataset)
     epoch_recon_loss = total_recon_loss / (2 * len_train_ds)
     epoch_loss = total_loss / (2 * len_train_ds)
+    model.eval()
     with torch.no_grad():
       # Just evaluate the reconstruction loss for the validation set
       assert(len(dl_validation.dataset) > 0) # TODO allow no validation
-      model.eval()
-      total_loss = 0.
-      mask_mass = 0.
-      displ_length = 0.
+      metrics = torch.zeros((4))
       for im1,im2 in dl_validation:
         batch_size = im1.shape[0]
         im1, im2 = im1.to(device), im2.to(device)
         input = torch.cat((im1, im2), dim=1)
         output, mask, flow, displacement = model(input)
-        loss = sfmnet.dssim_loss(im2, output)
+        loss = sfmnet.dssim_loss(im2, output, reduction=torch.sum)
 
-        total_loss    += loss * batch_size
-        mask_mass     += torch.sum(torch.mean(mask, dim=(1,)))
-        displ_length  += torch.sum(torch.mean(torch.abs(displacement), dim=(1,)))
-      avg_loss = total_loss / len_validate_ds
-      avg_mask_mass = mask_mass / len_validate_ds
-      avg_displ_length = displ_length / len_validate_ds
-      model.train()
-
-    epoch_callback(epoch=e, step=step, metric={
-      'Loss/Train/Recon': epoch_recon_loss,
-      'Loss/Train/Total': epoch_loss,
-      'Loss/Validation/Recon': avg_loss,
-      'Mask_Mass/Validation': avg_mask_mass,
-      'Displ_Length/Validation': avg_displ_length
-    })
+        metrics[0]   += loss
+        metrics[1]   += torch.sum(torch.mean(mask, dim=(1,)))
+        metrics[2]   += torch.sum(torch.mean(torch.abs(displacement), dim=(1,)))
+        metrics[3]   += 0 # TODO mask_var
+    model.train()
+    if using_ddp:
+      dist.reduce(metrics, 0)
+    if not using_ddp or dist.get_rank() is 0:
+      avg_loss, avg_mask_mass, avg_displ_length, avg_mask_var = metrics / len_validate_ds
+      epoch_callback(epoch=e, step=step, metric={
+        'Loss/Train/Recon': epoch_recon_loss,
+        'Loss/Train/Total': epoch_loss,
+        'Loss/Validation/Recon': avg_loss,
+        'Metric/MaskMass': avg_mask_mass,
+        'Metric/DisplLength': avg_displ_length,
+        'Metric/MaskVar': avg_mask_var,
+      })
+    if using_ddp:
+      dist.barrier()
 
   return model
 
@@ -139,51 +150,79 @@ def train(*,
   load_data_to_device=False,
   dl_num_workers=6,
   validation_split=0.1,
-  disable_cuda=False,
+
   K=1,
   C=16,
   fc_layer_width=128,
   num_hidden_layers=1,
   conv_depth=2,
+
   lr=0.001,
   flowreg_coeff=0.,
   maskreg_coeff=0.,
   displreg_coeff=0.,
   forwbackwreg_coeff=0.,
   maskvarreg_coeff=0.,
-  batch_size=16, 
+  batch_size=16,
+
   num_epochs=1, 
   n_vis_point=None,
   vis_freq=50,
+
+  local_rank=0,
+  local_world_size=1,
+  using_ddp=False,
 ):
   args = locals()
   pprint.PrettyPrinter(indent=4).pprint(args)
-  if disable_cuda is False and torch.cuda.is_available():
-    device = torch.device('cuda')
-  else:                         
-    device = torch.device('cpu')
-  print('training on ' + device.type)
 
   ds=PairConsecutiveFramesDataset(data_dir, load_all=load_data_to_device, device=device)
-  n_validation = int(len(ds) * validation_split)
-  n_train = len(ds) - n_validation
-  print(f'Validation size {n_validation}, train size {n_train}')
-  ds_train, ds_validation = torch.utils.data.random_split(ds, [n_train, n_validation], generator=torch.Generator().manual_seed(42))
-  dl_train = torch.utils.data.DataLoader(ds_train, batch_size=batch_size, shuffle=True, num_workers=dl_num_workers, pin_memory=True)
-  dl_validation = torch.utils.data.DataLoader(ds_validation, batch_size = 2 * batch_size, shuffle=False, num_workers=dl_num_workers, pin_memory=True)
   im_channels, H, W = ds[0][0].shape
-
   model = sfmnet.SfMNet(H=H, W=W, im_channels=im_channels, \
     C=C, K=K, conv_depth=conv_depth, \
     hidden_layer_widths=[fc_layer_width]*num_hidden_layers \
   )
-  model.to(device)
-  optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-  
-  if checkpoint_file is not None:
-    load(checkpoint_file, model, optimizer)
 
-  if tensorboard_dir is not None:
+  if using_ddp:
+    setup_dist(local_rank, local_world_size)
+    rank = dist.get_rank()
+
+    ngpus = torch.cuda.device_count() // local_world_size
+    device_ids = list(range(local_rank * ngpus, (local_rank + 1) * ngpus))
+    print(
+          f"[{os.getpid()}] rank = {dist.get_rank()}, "
+          + f"world_size = {dist.get_world_size()}, n = {ngpus}, device_ids = {device_ids}"
+    )
+    device = torch.device('cuda', device_ids[0])
+    model = DDP(model.cuda(device), device_ids)
+  elif torch.cuda.is_available():
+    rank = 0
+    device = torch.device('cuda', 0)
+    model = model.to(device)
+  else:
+    rank = 0
+    device = torch.device('cpu')
+
+  print('training on ' + device)
+
+  optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+  if checkpoint_file is not None:
+    load(checkpoint_file, model, optimizer, rank)
+
+  n_validation = int(len(ds) * validation_split)
+  n_train = len(ds) - n_validation
+  print(f'Validation size {n_validation}, train size {n_train}')
+  ds_train, ds_validation = torch.utils.data.random_split(ds, [n_train, n_validation], generator=torch.Generator().manual_seed(42))
+
+  sampler_train = torch.utils.data.DistributedSampler(ds_train) if using_ddp else None
+  sampler_validation = torch.utils.data.DistributedSampler(ds_validation, shuffle=False) if using_ddp else None
+  dl_train = torch.utils.data.DataLoader(ds_train, batch_size=batch_size, 
+    shuffle=(sampler_train is None), sampler=sampler_train, num_workers=dl_num_workers, pin_memory=True)
+
+  dl_validation = torch.utils.data.DataLoader(ds_validation, sampler=sampler_validation, 
+    batch_size=2*batch_size, shuffle=False, num_workers=dl_num_workers, pin_memory=True)
+
+  if tensorboard_dir is not None and rank is 0:
     sample_input = torch.cat((ds[0][0], ds[0][1]))
     writer = SummaryWriter(log_dir=tensorboard_dir)
     writer.add_graph(model, sample_input.to(device).unsqueeze(0))
@@ -210,7 +249,7 @@ def train(*,
       for k,v in metric.items():
         writer.add_scalar(k, v, step)
     if checkpoint_file is not None and epoch % checkpoint_freq == 0:
-      save(checkpoint_file, model, optimizer)
+      save(checkpoint_file, model, optimizer, rank)
     if writer is not None and vis_point is not None and epoch % vis_freq == 0:
       model.eval()
       fig = sfmnet.visualize(model, *vis_point)
@@ -229,7 +268,8 @@ def train(*,
     displreg_coeff=displreg_coeff,
     forwbackwreg_coeff=forwbackwreg_coeff,
     num_epochs=num_epochs,
-    epoch_callback=epoch_callback
+    epoch_callback=epoch_callback,
+    using_ddp=using_ddp
   )
 
   if writer is not None:
@@ -244,8 +284,39 @@ def train(*,
     })
 
   if checkpoint_file is not None:
-    save(checkpoint_file, model, optimizer)
+    save(checkpoint_file, model, optimizer, rank)
+  if using_ddp:
+    cleanup_dist()
   # return model
 
+
+def setup_dist(rank, world_size):
+  env_dict = {
+        key: os.environ[key]
+        for key in ("MASTER_ADDR", "MASTER_PORT", "RANK", "WORLD_SIZE")
+  }
+  # TODO - remove this
+  print(f"[{os.getpid()}] Initializing process group with: {env_dict}")
+  dist.init_process_group(backend="nccl", init_method='env://')
+  print(
+      f"[{os.getpid()}] world_size = {dist.get_world_size()}, "
+      + f"rank = {dist.get_rank()}, backend={dist.get_backend()}"
+  )
+
+def cleanup_dist():
+    dist.destroy_process_group()
+
 if __name__=='__main__':
-  m = fire.Fire(train)
+  n_gpus = torch.cuda.device_count()
+  if n_gpus <= 1:
+    m = fire.Fire(train)
+  else:
+    def run(rank, world_size):
+      print(f'I am {rank}/{world_size}')
+      setup(rank, world_size)
+      fire.Fire(functools.partial(using_ddp=True, rank=rank))
+    mp.spawn(run,
+        args=(n_gpus,),
+        nprocs=n_gpus,
+        join=True)
+  
