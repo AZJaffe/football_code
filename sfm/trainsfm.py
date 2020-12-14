a0bd073bb13fe9a813cef14cd3ad813e43f83927
Adam Jaffe <azjaffe1@gmail.com>
2020-12-14 17:05:13 -0500

    Add print statements to help debugging device

--- sfm/trainsfm.py
+++ sfm/trainsfm.py
@@ -369,9 +369,6 @@ def train(*,
       validation_model.train()
       if writer is not None:
         writer.add_figure(f'Visualization', fig, step)
-      else:
-        pass
-        # plt.show()
 
   train_loop(
     device=device,
